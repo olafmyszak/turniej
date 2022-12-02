bd9cd04420a1413076b7aa5e14843d42d4691602
Olaf Myszak <myszakolaf@gmail.com>
2022-12-02 17:19:24 +0100

    added some filthy stuff

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-class Warrior
+struct Warrior
 {
- public:
+  unsigned int id;
   unsigned int x;
   unsigned int y;
   unsigned int strength;
@@ -17,6 +17,47 @@ bool compareByX(const Warrior& a, const Warrior& b)
 	return (a.x < b.x);
 }
 
+Warrior calculateLoss(const Warrior& a, const Warrior& b)
+{
+	if(a.strength < b.strength)
+	{
+		return a;
+	}
+
+	if(a.strength > b.strength)
+	{
+		return b;
+	}
+
+	if(a.id > b.id)
+	{
+		return a;
+	}
+
+	return b;
+}
+
+void printWarrior(const Warrior& warrior)
+{
+	cout<<warrior.x<<" "<<warrior.y;
+}
+
+Warrior whoDies(const vector<Warrior>& warriors)
+{
+	size_t size = warriors.size();
+
+	if(size == 1)
+	{
+		return warriors[0];
+	}
+
+	if(size == 2)
+	{
+		return calculateLoss(warriors[0], warriors[1]);
+	}
+
+
+}
 
 int main()
 {
@@ -28,11 +69,10 @@ int main()
 	for(unsigned int i = 0; i<n; ++i)
 	{
 		cin >> warriors[i].x >> warriors[i].y >> warriors[i].strength;
+		warriors[i].id = i;
 	}
 
-
-
-
+	std::sort(warriors.begin(), warriors.end(), &compareByX);
 
 	return 0;
 }
